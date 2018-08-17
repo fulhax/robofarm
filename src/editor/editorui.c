631c235217ed567a14269714e88fe0886a304c0f
Fredrik Hansson <fredrikhansson_12345@yahoo.com>
2018-08-17 15:21:51 +0200

    started to work on the actual ui

--- src/editor/editorui.c
+++ src/editor/editorui.c
@@ -23,6 +23,12 @@ struct ui_tiles
     int numfiles;
 } ui_tiles;
 
+struct tile_type
+{
+    char name[64];
+} tiletypes[256] = {{0}};
+unsigned char numtiletypes = 0;
+
 int pngfilter(const struct dirent* entry)
 {
     char* ext = strrchr(entry->d_name, '.');
@@ -88,53 +94,61 @@ void ui_render()
     nk_ui_render();
 }
 
+enum
+{
+    NORMAL_STATE,
+    EDIT_TYPE
+};
+int state = NORMAL_STATE;
+void ui_style_disabled(char status)
+{
+    if(status)
+    {
+        nk_style_default(ctx);
+    }
+    else
+    {
+        ctx->style.button.normal = nk_style_item_color(nk_rgb(40, 40, 40));
+        ctx->style.button.hover = nk_style_item_color(nk_rgb(40, 40, 40));
+        ctx->style.button.active = nk_style_item_color(nk_rgb(40, 40, 40));
+        ctx->style.button.border_color = nk_rgb(60, 60, 60);
+        ctx->style.button.text_background = nk_rgb(60, 60, 60);
+        ctx->style.button.text_normal = nk_rgb(60, 60, 60);
+        ctx->style.button.text_hover = nk_rgb(60, 60, 60);
+        ctx->style.button.text_active = nk_rgb(60, 60, 60);
+    }
+}
 void ui_logic()
 {
     nk_ui_update();
+    ui_style_disabled(state == NORMAL_STATE);
 
-    if(nk_begin(ctx, "", nk_rect(options.width - 250, 0, 250, options.height), NK_WINDOW_BORDER))
+    if(nk_begin(ctx, "main window", nk_rect(options.width - 250, 0, 250, options.height), NK_WINDOW_BORDER))
     {
-        enum {EASY, HARD};
-        static int op = EASY;
-        static int property = 20;
-        nk_layout_row_static(ctx, 30, 80, 1);
+        nk_layout_row_dynamic(ctx, 16, 1);
 
-        if(nk_button_label(ctx, "button"))
+        if(nk_button_label(ctx, "new type"))
         {
-            fprintf(stdout, "button pressed\n");
+            state = EDIT_TYPE;
         }
 
-        nk_layout_row_static(ctx, 48, 48, 1);
+        nk_layout_row_dynamic(ctx, 200, 1);
+        nk_style_push_color(ctx, &ctx->style.window.border_color, nk_rgba_f(0.5, 0, 0, 1));
+        nk_style_push_float(ctx, &ctx->style.window.border, 20);
+        nk_group_begin(ctx, "type list", NK_WINDOW_BACKGROUND | NK_WINDOW_BORDER);
+        nk_layout_row_dynamic(ctx, 32, 1);
 
-        if(nk_button_image(ctx, ui_images.buttontest))
+        for(int i = 0; i < numtiletypes; i++)
         {
-            fprintf(stdout, "button pressed\n");
-        }
-
-        nk_layout_row_static(ctx, 32, 32, 6);
-
-        for(int i = 0; i < ui_tiles.numfiles; i++)
-        {
-            if(nk_button_image(ctx, ui_images.buttontest))
+            if(nk_button_image_label(ctx, ui_images.buttontest, tiletypes[i].name, NK_TEXT_ALIGN_RIGHT) && state == NORMAL_STATE)
             {
-                fprintf(stdout, "button pressed\n");
+                fprintf(stdout, "button pressed type:%i\n", i);
             }
         }
 
-        nk_layout_row_dynamic(ctx, 30, 2);
-
-        if(nk_option_label(ctx, "easy", op == EASY))
-        {
-            op = EASY;
-        }
-
-        if(nk_option_label(ctx, "hard", op == HARD))
-        {
-            op = HARD;
-        }
-
-        nk_layout_row_dynamic(ctx, 25, 1);
-        nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
+        nk_group_end(ctx);
+        nk_style_pop_color(ctx);
+        nk_style_pop_float(ctx);
         nk_layout_row_dynamic(ctx, 20, 1);
         nk_label(ctx, "background:", NK_TEXT_LEFT);
         nk_layout_row_dynamic(ctx, 25, 1);
@@ -150,7 +164,36 @@ void ui_logic()
             bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
             nk_combo_end(ctx);
         }
+
+        nk_end(ctx);
     }
 
-    nk_end(ctx);
+    ui_style_disabled(state == EDIT_TYPE);
+
+    if(state == EDIT_TYPE)
+    {
+        if(nk_begin(ctx, "edit type", nk_rect(50, options.height / 2 - 100, 250, 200), NK_WINDOW_BORDER | NK_WINDOW_TITLE | NK_WINDOW_MOVABLE))
+        {
+            static char typename[64] = {0};
+            nk_layout_row_dynamic(ctx, 32, 1);
+            nk_edit_string_zero_terminated(ctx, NK_EDIT_BOX | NK_EDIT_AUTO_SELECT, typename, 64, nk_filter_ascii);
+            nk_layout_row_dynamic(ctx, 16, 2);
+
+            if(nk_button_label(ctx, "canel"))
+            {
+                state = NORMAL_STATE;
+                typename[0] = 0;
+            }
+
+            if(nk_button_label(ctx, "save"))
+            {
+                state = NORMAL_STATE;
+                snprintf(tiletypes[numtiletypes].name, 64, "%s", typename);
+                numtiletypes++;
+                typename[0] = 0;
+            }
+        }
+
+        nk_end(ctx);
+    }
 }
